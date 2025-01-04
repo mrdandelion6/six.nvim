0497a2f5c361cd659e483cb01fa21de370028bf4
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2025-01-03 22:41:54 -0500

    figured out how to read signals from child terminals using OSC; updated create_terminal_pwd_update() accordingly, and added a prase_terminal_request() function to clean the message string.

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -7,21 +7,29 @@ MOST_RECENT_PWD = vim.fn.expand '~/.config/nvim/bash/recent_pwd.txt'
 -- keeps track if we already created
 local created_terminal_pwd_update = {}
 
-print('A: table[buf] is ' .. tostring(created_terminal_pwd_update[19]))
+local function parse_terminal_request(request)
+  -- the bash code that sends terminal requests with OSC is as follows:
+  --  printf '\033]51;%s\007' $(pwd)
+  -- i have tried many other variations as well, but was not able to get a clean transfer of json or string.
+  -- the requests (collected with vim.v.termrequest), end up having some leading characters.
+  -- to make a quick work around for this with my current config, i am just going to splice them off.
+  -- hopefully one day i can figure out how to properly send/read the OSC requests so i don't need abitrary cleaning like this.
+  return request:sub(6)
+end
 
 local function create_terminal_pwd_update(buf)
   print('trigger 1. buf is ' .. buf)
-  print('table[buf] is ' .. tostring(created_terminal_pwd_update[buf]))
+  print('table[' .. buf .. '] is ' .. tostring(created_terminal_pwd_update[buf]))
   if not created_terminal_pwd_update[buf] then
     print 'trigger 2'
-    vim.api.nvim_create_autocmd({ 'FileChangedShellPost' }, {
-      pattern = MOST_RECENT_PWD,
+    vim.api.nvim_create_autocmd({ 'TermRequest' }, {
       callback = function()
-        print 'trigger 3'
+        local request = parse_terminal_request(vim.v.termrequest)
+        print('trigger 3, request is: ' .. request)
         if vim.api.nvim_buf_is_valid(buf) then
-          print 'cwd changed!!!'
+          print 'trigger 4'
           vim.api.nvim_buf_call(buf, function()
-            print 'hi'
+            print 'trigger 5'
           end)
         end
       end,
@@ -40,16 +48,12 @@ end
 
 local term_upd_group = vim.api.nvim_create_augroup('autoload_cmd_lualine_updates', { clear = true })
 
-print('B: table[buf] is ' .. tostring(created_terminal_pwd_update[19]))
-
 vim.api.nvim_create_autocmd('TermOpen', {
   group = term_upd_group,
   callback = function(opts)
     create_terminal_pwd_update(opts.buf)
   end,
 })
-
-print('C: table[buf] is ' .. tostring(created_terminal_pwd_update[19]))
 
 vim.api.nvim_create_autocmd('TermClose', {
   group = term_upd_group,
@@ -58,8 +62,6 @@ vim.api.nvim_create_autocmd('TermClose', {
   end,
 })
 
-print('D: table[buf] is ' .. tostring(created_terminal_pwd_update[19]))
-
 vim.api.nvim_create_autocmd('VimEnter', {
   group = term_upd_group,
   callback = function()
@@ -67,5 +69,3 @@ vim.api.nvim_create_autocmd('VimEnter', {
     create_terminal_pwd_updates_for_all()
   end,
 })
-
-print('E: table[buf] is ' .. tostring(created_terminal_pwd_update[19]))
