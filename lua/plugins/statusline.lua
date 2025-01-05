2c5ced0c339cb1fab8eaf1af0ae4e689d1d89fa3
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2025-01-05 04:37:02 -0500

    inactivte buffers now display the same file name as activte buffers

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -145,6 +145,27 @@ return {
           },
         },
       },
+      inactive_sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+            file_status = true,
+            fmt = function(name) -- for terminal, display path of termrinal
+              if vim.bo.buftype == 'terminal' then
+                local buf = vim.api.nvim_get_current_buf()
+                -- use the local buffer pwd which we set in after/plugin/lualine.lua
+                if vim.b[buf].terminal_pwd and vim.b[buf].terminal_pwd ~= '' then
+                  return vim.b[buf].terminal_pwd
+                end
+                return vim.b.term_title:match 'term://(.-)//' or name
+              end
+              return name
+            end,
+            padding = { left = 1, right = 1 },
+          },
+        },
+      },
     }
     vim.opt.termguicolors = true
   end,
