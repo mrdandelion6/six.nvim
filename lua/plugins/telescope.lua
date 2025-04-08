131c183e7ce53cb3a0a2d3a2aad01ccac4874fb7
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2025-04-08 16:17:59 -0400

    leader fe now also uses same opts as leader ff. factored out opts table into a get_opts() function for repeated usage

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,14 @@
-return { -- Fuzzy Finder (files, lsp, etc)
+return { -- fuzzy finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+    { -- if encountering errors, see telescope-fzf-native readme for installation instructions
       'nvim-telescope/telescope-fzf-native.nvim',
 
       -- `build` is used to run some command when the plugin is installed/updated.
-      -- This is only run then, not every time Neovim starts up.
+      -- this is only run then, not every time neovim starts up.
       build = 'make',
 
       -- `cond` is a condition used to determine whether this plugin should be
@@ -19,19 +19,19 @@ return { -- Fuzzy Finder (files, lsp, etc)
     },
     { 'nvim-telescope/telescope-ui-select.nvim' },
 
-    -- Useful for getting pretty icons, but requires a Nerd Font.
+    -- useful for getting pretty icons, but requires a nerd font.
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
 
   config = function()
-    -- Telescope is a fuzzy finder that comes with a lot of different things.
+    -- telescope is a fuzzy finder that comes with a lot of different things.
     --  :Telescope help_tags
 
-    -- [[ Configure Telescope ]]
-    -- See `:help telescope` and `:help telescope.setup()`
+    -- [[ configure telescope ]]
+    -- see `:help telescope` and `:help telescope.setup()`
     require('telescope').setup {
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
+      -- you can put your default mappings / updates / etc. in here
+      --  all the info you're looking for is in `:help telescope.setup()`
       --
       -- defaults = {
       --   mappings = {
@@ -46,51 +46,18 @@ return { -- Fuzzy Finder (files, lsp, etc)
       },
     }
 
-    -- Enable Telescope extensions if they are installed
+    -- enable telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
 
-    -- See `:help telescope.builtin`
+    -- see `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
 
     vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg = '#e4abd4' })
 
-    --
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
-
-    -- Find vim keymaps
-    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
-
-    -- Find different telescope pickers. For example, `find_files` picker which is '<leader>ff'.
-    vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[F]ind [S]pecific finder' })
-
-    -- Find the word currently under the cursor in your buffer in same picker as <leader>fg but pre-populated.
-    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch [W]ord under cursor' })
-
-    -- Search text inside files within PWD
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
-
-    -- Search diagnostics and warnings within current buffer
-    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-
-    -- Repeat last search
-    vim.keymap.set('n', '<leader>fa', builtin.resume, { desc = '[F]ind [A]gain' })
-
-    -- Search recently opened files
-    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = '[F]ind [R]ecently viewed' })
-
-    -- Search for existing buffers
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-    -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set('n', '<leader>fe', function()
-      builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[F]ind N[e]ovim files' })
-
-    vim.keymap.set('n', '<leader>ff', function()
-      -- Fuzzy search for files in PWD.
-      -- Attempts to use fdfind or fd if former not available.
-      -- If neither available, defaults to Vim's built in file finding functions.
+    local function get_opts()
+      -- attempts to use fdfind or fd if former not available.
+      -- if neither available, defaults to vim's built in file finding functions.
       local opts = {
         hidden = true,
       }
@@ -116,7 +83,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
         }
         vim.notify 'Using ripgrep'
       elseif vim.fn.executable 'fdfind' == 1 then
-        opts.find_command = { -- Ignore .git/, node_modules/, and other directories we typically wouldn't search.
+        opts.find_command = { -- ignore .git/, node_modules/, and other directories we typically wouldn't search.
           'fdfind',
           '--type',
           'f',
@@ -160,19 +127,65 @@ return { -- Fuzzy Finder (files, lsp, etc)
         }
         vim.notify('Using default finder', vim.log.levels.INFO)
       end
+      return opts
+    end
+
+    --
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+
+    -- find vim keymaps
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
+
+    -- find different telescope pickers. for example, `find_files` picker which is '<leader>ff'.
+    vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[F]ind [S]pecific finder' })
+
+    -- find the word currently under the cursor in your buffer in same picker as <leader>fg but pre-populated.
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch [W]ord under cursor' })
+
+    -- search text inside files within pwd
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+
+    -- search diagnostics and warnings within current buffer
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+
+    -- repeat last search
+    vim.keymap.set('n', '<leader>fa', builtin.resume, { desc = '[F]ind [A]gain' })
+
+    -- search recently opened files
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = '[F]ind [R]ecently viewed' })
+
+    -- search for existing buffers
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+    -- shortcut for searching your neovim configuration files
+    vim.keymap.set('n', '<leader>fe', function()
+      local opts = get_opts()
+      opts.cwd = vim.fn.stdpath 'config'
       builtin.find_files(opts)
+    end, { desc = '[F]ind N[e]ovim files' })
+
+    -- shortcut for searching your neovim configuration files
+    vim.keymap.set('n', '<leader>fn', function()
+      local opts = get_opts()
+      opts.cwd = vim.fn.stdpath 'config'
+      builtin.find_files(opts)
+    end, { desc = '[F]ind N[e]ovim files' })
+
+    -- fuzzy search for files in pwd.
+    vim.keymap.set('n', '<leader>ff', function()
+      builtin.find_files(get_opts())
     end, { desc = '[F]ind [F]iles' })
 
-    -- Slightly advanced example of overriding default behavior and theme
+    -- slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+      -- you can pass additional configuration to telescope to change the theme, layout, etc.
       builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
         previewer = false,
       })
     end, { desc = '[/] Fuzzily search in current buffer' })
 
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
+    -- it's also possible to pass additional configuration options.
+    --  see `:help telescope.builtin.live_grep()` for information about particular keys
     vim.keymap.set('n', '<leader>f/', function()
       builtin.live_grep {
         grep_open_files = true,
