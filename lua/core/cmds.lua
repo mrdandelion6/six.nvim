2d0814923ddcf05eba25ccf80b5822feb5c57193
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2025-04-08 20:06:33 -0400

    added user command to write a file without auto formatting

--- lua/core/cmds.lua
+++ lua/core/cmds.lua
@@ -1,1 +1,8 @@
 vim.cmd 'command! -nargs=1 Vr vertical resize <args>'
+vim.api.nvim_create_user_command('Ww', function()
+  -- write without autoformat
+  local prev_state = vim.g.format_on_save
+  vim.g.format_on_save = false
+  vim.cmd 'write'
+  vim.g.format_on_save = prev_state
+end, {})
