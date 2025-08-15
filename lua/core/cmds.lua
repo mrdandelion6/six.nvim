9117e10d68fcb43b31a5062f3ff91acfd8c90bdf
faisal shaik <faisal.shaik@amd.com>
2025-08-15 13:18:09 -0400

    code refactor [cmds.lua]: moved command for write buffer without save to autoformat.lua and renamed command from :Ww to :W

--- lua/core/cmds.lua
+++ lua/core/cmds.lua
@@ -1,8 +1,4 @@
 vim.cmd 'command! -nargs=1 Vr vertical resize <args>'
-vim.api.nvim_create_user_command('Ww', function()
-  -- write without autoformat
-  local prev_state = vim.g.format_on_save
-  vim.g.format_on_save = false
-  vim.cmd 'write'
-  vim.g.format_on_save = prev_state
-end, {})
+vim.cmd 'command! -nargs=1 Hr horizontal resize <args>'
+
+-- TODO: add commands and keymaps for C++ compilation
