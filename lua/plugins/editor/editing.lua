088334abcad5d5e0248000848d92f683a05de2ec
faisal shaik <faisal.shaik@amd.com>
2025-08-16 20:53:54 -0400

    remove plugin [mini.nvim]: was barely using

--- lua/plugins/editor/editing.lua
+++ lua/plugins/editor/editing.lua
@@ -9,31 +9,4 @@ return {
 
   -- highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-  { -- collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- better around/inside textobjects
-      --
-      -- examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup {
-        n_lines = 500,
-        mappings = { -- disabled these to prevent conflict with colemak-dh mappings
-          inside_last = '',
-          inside_next = '',
-          inside = '',
-        },
-      }
-
-      -- add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-    end,
-  },
 }
