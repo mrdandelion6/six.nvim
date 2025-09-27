d800ba9a15bb5611339c60998999012bee35230d
faisal shaik <faisal.shaik@amd.com>
2025-09-26 20:05:36 -0400

    feat [telescope]: added some new user commands for calling telescope on a specific path

--- lua/plugins/nav/telescope.lua
+++ lua/plugins/nav/telescope.lua
@@ -1,4 +1,4 @@
-return { -- fuzzy finder (files, lsp, etc)
+return {
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
   branch = '0.1.x',
@@ -55,8 +55,16 @@ return { -- fuzzy finder (files, lsp, etc)
     vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg = '#e4abd4' })
 
     local function get_opts()
-      -- attempts to use fdfind or fd if former not available.
-      -- if neither available, defaults to vim's built in file finding functions.
+      -- return a table of options for telescope fuzzy searching. we need this
+      -- to determine what finder backend we are using and the proper commands
+      -- for them.
+
+      -- attempts to use the following finders in order:
+      --  1. ripgrep
+      --  2. fdfind
+      --  3. fd
+      --  4. vim's built in file funding functions
+
       local opts = {
         hidden = true,
       }
@@ -82,55 +90,112 @@ return { -- fuzzy finder (files, lsp, etc)
           '--glob',
           '!build',
         }
-      elseif vim.fn.executable 'fdfind' == 1 then
-        opts.find_command = { -- ignore .git/, node_modules/, and other directories we typically wouldn't search.
-          'fdfind',
-          '--type',
-          'f',
-          '--hidden',
-          '--no-ignore-vcs',
-          '--exclude',
-          '.git',
-          '--exclude',
-          'node_modules',
-          '--exclude',
-          'target',
-          '--exclude',
-          'dist',
-          '--exclude',
-          '.*/',
-        }
-        vim.notify('Using fd-find', vim.log.levels.INFO)
-      elseif vim.fn.executable 'fd' == 1 then
-        opts.find_command = {
-          'fd',
-          '--type',
-          'f',
-          '--exclude',
-          '.git',
-          '--exclude',
-          'node_modules',
-          '--exclude',
-          'target',
-          '--exclude',
-          'dist',
-          '--exclude',
-          '.*/',
-        }
-        vim.notify('Using fd', vim.log.levels.INFO)
       else
-        opts.file_ignore_patterns = {
-          '^.git/',
-          'node_modules/',
-          'target/',
-          'dist/',
-        }
-        vim.notify('Using default finder', vim.log.levels.INFO)
+        vim.notify('WARNING (telescope.lua): ripgrep not found! ripgrep is the recommended finder to use.',
+          vim.log.levels.INFO)
+
+        if vim.fn.executable 'fdfind' == 1 then
+          opts.find_command = {
+            -- ignore .git/ , node_modules/ , etc.
+            'fdfind',
+            '--type',
+            'f',
+            '--hidden',
+            '--no-ignore-vcs',
+            '--exclude',
+            '.git',
+            '--exclude',
+            'node_modules',
+            '--exclude',
+            'target',
+            '--exclude',
+            'dist',
+            '--exclude',
+            '.*/',
+          }
+        elseif vim.fn.executable 'fd' == 1 then
+          opts.find_command = {
+            'fd',
+            '--type',
+            'f',
+            '--exclude',
+            '.git',
+            '--exclude',
+            'node_modules',
+            '--exclude',
+            'target',
+            '--exclude',
+            'dist',
+            '--exclude',
+            '.*/',
+          }
+        else
+          opts.file_ignore_patterns = {
+            '^.git/',
+            'node_modules/',
+            'target/',
+            'dist/',
+          }
+        end
       end
       return opts
     end
 
-    --
+    local function fuzzy_find_in_path(path, fuzzy_command)
+      -- call the fuzzy_command with a specific path
+
+      local ts_opts = get_opts()
+
+      if path and path ~= '' then
+        local expanded_path = vim.fn.expand(path)
+        if vim.fn.isdirectory(expanded_path) == 1 then
+          ts_opts.cwd = expanded_path
+        else
+          vim.notify("Directory does not exist: " .. expanded_path, vim.log.levels.ERROR)
+          return
+        end
+      end
+
+      fuzzy_command(ts_opts)
+    end
+
+    --[[
+    ********************
+          COMMANDS
+    ********************
+    --]]
+
+    vim.api.nvim_create_user_command('Ff', function(opts)
+      fuzzy_find_in_path(opts.args, builtin.find_files)
+    end, { nargs = 1 })
+
+    vim.api.nvim_create_user_command('Fg', function(opts)
+      fuzzy_find_in_path(opts.args, builtin.live_grep)
+    end, { nargs = 1 })
+
+    vim.api.nvim_create_user_command('UpdateTelescopeMaps', function()
+      local new_maps = vim.g.telescope_maps or {}
+      require('telescope').setup {
+        defaults = {
+          mappings = new_maps,
+        },
+      }
+    end, {})
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'TelescopeMapsChanged',
+      callback = function()
+        vim.cmd 'UpdateTelescopeMaps'
+      end,
+    })
+
+    --[[
+    ********************
+          KEYMAPS
+    ********************
+    --]]
+
+    -- help
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
 
     -- find vim keymaps
@@ -164,13 +229,12 @@ return { -- fuzzy finder (files, lsp, etc)
       builtin.find_files(opts)
     end, { desc = '[F]ind N[e]ovim files' })
 
-    -- shortcut for searching files in my Learn_to_Code repository
+    -- fuzzy search in my learn_to_code repository
     vim.keymap.set('n', '<leader>fn', function()
       if vim.g.local_settings then
         if vim.g.local_settings.notes_path then
           local opts = get_opts()
           opts.cwd = vim.g.local_settings.notes_path
-          print('notes path is: ' .. vim.g.local_settings.notes_path)
           builtin.find_files(opts)
         else
           print 'ERROR (telescope.lua): vim.g.local_settings.notes_path is nil'
@@ -185,7 +249,7 @@ return { -- fuzzy finder (files, lsp, etc)
       builtin.find_files(get_opts())
     end, { desc = '[F]ind [F]iles' })
 
-    -- slightly advanced example of overriding default behavior and theme
+    -- fuzzy find in current buffer.. like regular / but with a menu
     vim.keymap.set('n', '<leader>/', function()
       -- you can pass additional configuration to telescope to change the theme, layout, etc.
       builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -193,8 +257,7 @@ return { -- fuzzy finder (files, lsp, etc)
       })
     end, { desc = '[/] Fuzzily search in current buffer' })
 
-    -- it's also possible to pass additional configuration options.
-    --  see `:help telescope.builtin.live_grep()` for information about particular keys
+    --  fuzzy grep open files
     vim.keymap.set('n', '<leader>f/', function()
       builtin.live_grep {
         grep_open_files = true,
@@ -202,21 +265,6 @@ return { -- fuzzy finder (files, lsp, etc)
       }
     end, { desc = '[F]ind [/] in Open Files' })
 
-    vim.api.nvim_create_user_command('UpdateTelescopeMaps', function()
-      local new_maps = vim.g.telescope_maps or {}
-      require('telescope').setup {
-        defaults = {
-          mappings = new_maps,
-        },
-      }
-    end, {})
-
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'TelescopeMapsChanged',
-      callback = function()
-        vim.cmd 'UpdateTelescopeMaps'
-      end,
-    })
 
     vim.cmd 'UpdateTelescopeMaps' -- run once on load
   end,
