3c26ebc9023d13e28a80df895366412cabe0a349
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2025-11-15 00:14:21 -0500

    feat[discord]: added icons for cuda and hip files for discord presence. also added cpp as the temporary filetype for hip files until i get proper rocm setup.. need to make more space on my pc first

--- lua/core/filetype.lua
+++ lua/core/filetype.lua
@@ -3,5 +3,7 @@ vim.filetype.add {
     qmd = 'quarto',
     cu = 'cuda',
     cuh = 'cuda',
+    -- HACK: change ths to hip once you have set up lsp support for it
+    hip = 'cpp',
   },
 }
--- lua/plugins/integrations/discord.lua
+++ lua/plugins/integrations/discord.lua
@@ -1,10 +1,16 @@
 return {
   'andweeb/presence.nvim',
   config = function()
+    local dev_icons = 'https://raw.githubusercontent.com/mrdandelion6/assets/main/dev-icons/'
     require('presence').setup {
       auto_update = true,
       neovim_image_text = 'check out my config... https://github.com/mrdandelion6/six.nvim',
       main_image = 'neovim',
+      file_assets = {
+        cu = { 'CUDA', dev_icons .. 'nvidia.png' },
+        cuh = { 'CUDA Header', dev_icons .. 'nvidia.png' },
+        hip = { 'HIP', dev_icons .. 'amd.png' },
+      },
     }
   end,
 }
