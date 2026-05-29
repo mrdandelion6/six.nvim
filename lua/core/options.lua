ce15f1fa7e9dfbac70e08786b429196191e08558
mrdandelion6 <fashaik@nvidia.com>
2026-05-29 02:13:22 -0700

    feat[clipboard]: now have OS52 pasting for ssh sessions

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -12,6 +12,21 @@ vim.opt.showmode = false
 -- schedule the setting after `uienter` because it can increase startup-time.
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
+
+  -- osc 52 clipboard — works over ssh with no extra tools
+  if vim.env.SSH_TTY ~= nil or vim.env.SSH_CONNECTION ~= nil then
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+        ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+      },
+      paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+        ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+      },
+    }
+  end
 end)
 
 -- disable netrw completely
