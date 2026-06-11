11458d41872f931a2b1a0946654c687a89f2e5a5
mrdandelion6 <fashaik@nvidia.com>
2026-06-11 14:31:34 -0700

    refactor[commands]: deleted autocmds.lua file , now have both core commands and autocommands in core/cmds.lua. create cursor.lua file for cursor centering

--- lua/core/cmds.lua
+++ lua/core/cmds.lua
@@ -1,9 +1,34 @@
+-- NOTE: this file contains both autocommands and commands for core/ features.
+-- for autocommands that interact with plugins , see the lua files for those
+-- specific plugins.
+local utils = require 'core.utils'
+
+-- PANE
 vim.cmd 'command! -nargs=1 Vr vertical resize <args>'
 vim.cmd 'command! -nargs=1 Hr horizontal resize <args>'
+
+-- TEXT
+-- yank path of current buffer
 vim.api.nvim_create_user_command('Yp', function()
   local path = vim.fn.expand '%:p:h'
   vim.fn.setreg('+', path)
   print('Yanked path: ' .. path)
 end, {})
+
+-- highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- GIT
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  callback = function()
+    vim.g.git_root = utils.get_git_root()
+  end,
+})
 
 -- TODO: add commands and keymaps for C++ compilation
